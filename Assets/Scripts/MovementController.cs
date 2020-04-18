ee7f78d3e59e522211d38b211488a406bae9922a
Akramion <leha.bushuev@gmail.com>
2020-04-18 18:18:09 +0500

    Movement

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float moveLimiter = 0.7f;
+    private Rigidbody2D rigidbody;
+
+    private void Start() {
+        rigidbody = GetComponent<Rigidbody2D>(); 
+    }
+
+    public void Move(Vector2 movement) {
+        if (movement.x != 0 && movement.y != 0)
+        {
+            movement.x *= moveLimiter;
+            movement.y *= moveLimiter;
+        }
+
+         rigidbody.velocity = new Vector2(movement.x * moveSpeed, movement.y * moveSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float horizontal;
+    float vertical;
+    MovementController movementController;
+    void Start()
+    {
+        movementController = GetComponent<MovementController>(); 
+    }
+
+   
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical"); 
+    }
+
+    private void FixedUpdate() {
+        movementController.Move(new Vector2(horizontal, vertical));
+    }
+}
